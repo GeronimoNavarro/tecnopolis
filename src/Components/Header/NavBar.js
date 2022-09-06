5dccf43bc8b4de738ab65660b7dd08161af8c35e
Geronimo Navarro <geronimonavarro6@gmail.com>
2022-09-06 06:48:25 -0300

    Entrega NavBar

---
+++ src/Components/Header/NavBar.js
@@ -0,0 +1,22 @@
+import "./NavBar.css"
+
+
+const NavBar = () => {
+    return(
+        <>
+        <header>
+        <img src="" alt= "loguito" />
+        <h1>Tecnopolis</h1>
+        <nav>
+            <a href="">Productos</a>
+            <a href="">Servicios</a>
+            <a href="">Acerca de nosotros</a>
+            <a href="">Contacto</a>
+        </nav>
+        <img src="" alt="carrito" />
+        </header>
+        </>
+    )
+}
+
+export default NavBar
