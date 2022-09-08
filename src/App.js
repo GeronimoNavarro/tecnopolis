f6b16fae8d652e2b5c53e3bffcc8e8a1bf49ac51
Geronimo Navarro <geronimonavarro6@gmail.com>
2022-09-08 17:37:29 -0300

    TecnopolisNavarro2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
 import NavBar from "./Components/NavBar/NavBar";
+import ItemListContainer from "./ItemListContainer/ItemListContainer"
+
+
 
 
 const App = () =>{
+
+
+const mensaje = "Bienvenidos a TECNOPOLIS"
+
+
+
   return (
+  <>
   <NavBar />
+  <ItemListContainer greeting={mensaje} />
+  </>
   )
 }
 
