ebe4b3c736b459f70d0db8c603489ba5405f1c7c
Geronimo Navarro <geronimonavarro6@gmail.com>
2022-09-13 20:10:10 -0300

    ItemCount-Navarro

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import NavBar from "./Components/NavBar/NavBar";
 import ItemListContainer from "./ItemListContainer/ItemListContainer"
+import ItemCount from "./ItemCount"
 
 
 
@@ -10,12 +11,19 @@ const App = () =>{
 
 const mensaje = "Bienvenidos a TECNOPOLIS"
 
+const onAdd = (contador) => {
+  alert("Se agrego al carrito con exito");
+  console.log(contador)
 
+
+
+}
 
   return (
   <>
   <NavBar />
   <ItemListContainer greeting={mensaje} />
+  <ItemCount stock={30} onAdd={onAdd} initial={0} />
   </>
   )
 }
